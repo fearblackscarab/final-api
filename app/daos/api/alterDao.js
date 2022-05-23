a19f85edbffed1b43bee919092ffe4667e1f679a
FearBlackScarab <fearblackscarab@gmail.com>
2022-05-23 09:53:25 -0500

    final-api v1.0

--- app/daos/api/alterDao.js
+++ app/daos/api/alterDao.js
@@ -30,58 +30,6 @@ const alterDao = {
                 }
             )
         }
-    },
-
-    update: (req, res) => {
-        if (isNaN(req.params.id)) {
-            res.json({
-                "error": true,
-                "message": "Is not a number"
-            })
-        } else if (Object.keys(req.body) === 0) {
-            res.json({
-                "error": true,
-                "message": "No fields to update"
-            })
-        } else {
-            const fields = Object.keys(req.body);// creates field array
-            const values = Object.values(req.body);// creates value array
-
-            con.execute(
-                // string, array, callback
-                `update jewelry set ${fields.join('=?,')}=? where jewelry_id=?`,
-                [...values, req.params.id],
-                (error, dbres) => {
-                    if (!error) {
-                        if (dbres.length === 1) {
-                            res.send(`changed ${dbres.changedRows}`)
-                        } else {
-                            res.send(`changed ${dbres.changedRows}`)
-                        }
-                    } else {
-                        res.send('error updating')
-                        console.log('filmdao error', error)
-                    }
-                }
-            )
-        }
-    },
-
-    findByRating: (req, res) => {
-        con.execute(
-            'select * from jewelry where type=?',
-            [rating],
-            (error, rows) => {
-                if (!error) {
-                    if (rows.length === 1) {
-                        res.json(...rows)
-                    } else {
-                        res.json(rows)
-                    }
-                }
-            }
-
-        )
     }
 }
 
